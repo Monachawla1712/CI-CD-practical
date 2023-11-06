979621de5974afffde36773955f844c7cbed9ec4
Monachawla1712 <146841568+Monachawla1712@users.noreply.github.com>
2023-11-07 00:47:53 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', '' );
+define( 'DB_NAME', 'Databasename' );
 
 /** Database username */
-define( 'DB_USER', '' );
+define( 'DB_USER', 'User' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'Password' );
 
 /** Database hostname */
-define( 'DB_HOST', '' );
+define( 'DB_HOST', 'Host' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
