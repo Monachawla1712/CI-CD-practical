df42380823f863b6f1b22505910bae3214974cc8
Monachawla1712 <146841568+Monachawla1712@users.noreply.github.com>
2023-11-07 16:49:51 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
-
-export Databasename
-export User
-export Password
-export Host
-
-sudo sed -i "s/^.*DB_NAME.*$/define('DB_NAME', '$Databasename' );/" wp-config.php
-sudo sed -i "s/^.*DB_USER.*$/define('DB_USER, '$User' );/" wp-config.php
-sudo sed -i "s/^.*DB_PASSWORD.*$/define('DB_PASSWORD', '$Password' );/" wp-config.php
-sudo sed -i "s/^.*DB_HOST.*$/define('DB_HOST', '$Host' );/" wp-config.php
-
-
-
-
+echo hello
