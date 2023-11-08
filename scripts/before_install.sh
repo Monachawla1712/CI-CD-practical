d75eefdc2d57d9025379b7ba1ecd44a229fa3225
Monachawla1712 <146841568+Monachawla1712@users.noreply.github.com>
2023-11-08 09:28:29 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,1 +1,11 @@
-echo hellohiiiii
+echo hiii
+#!/bin/bash
+
+# Stop the Apache service if it's running
+if pgrep apache2 > /dev/null; then
+  sudo /etc/init.d/apache2 stop > /dev/null
+  echo "Apache service stopped."
+else
+  echo "No Apache processes found."
+fi
+
