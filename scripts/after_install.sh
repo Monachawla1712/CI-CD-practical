6715549fea452cfb0321e908722a2dfaea2c00ff
Monachawla1712 <146841568+Monachawla1712@users.noreply.github.com>
2023-11-08 09:32:01 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,1 +1,9 @@
-sudo service apache2 start
+#!/bin/bash
+
+# Start the Apache service if it's not running
+if ! pgrep apache2 > /dev/null; then
+  sudo /etc/init.d/apache2 start > /dev/null
+  echo "Apache service started."
+else
+  echo "Apache2 is already running."
+fi
