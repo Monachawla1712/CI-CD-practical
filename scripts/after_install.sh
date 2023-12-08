bc7959ea70ce72a6e6debd9eb23b7bccfc2c48e0
Monachawla1712 <146841568+Monachawla1712@users.noreply.github.com>
2023-12-08 17:16:54 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-# private_ip=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=new_db" --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
-# sudo sed -i "s/^.*DB_HOST.*$/define('DB_HOST', '$private_ip');/" /var/www/html/wp-config.php
+private_ip=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=new_db" --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+sudo sed -i "s/^.*DB_HOST.*$/define('DB_HOST', '$private_ip');/" /var/www/html/wp-config.php
 
 
-# Start the Apache service if it's not running
-if ! pgrep apache2 > /dev/null; then
-  sudo /etc/init.d/apache2 start > /dev/null
-  echo "Apache service started."
-else
-  echo "Apache2 is already running."
-fi
+# # Start the Apache service if it's not running
+# if ! pgrep apache2 > /dev/null; then
+#   sudo /etc/init.d/apache2 start > /dev/null
+#   echo "Apache service started."
+# else
+#   echo "Apache2 is already running."
+# fi
