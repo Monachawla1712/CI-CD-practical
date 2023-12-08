ee85a9a1670a4478cfb8b0042e06c9a6946b5668
Monachawla1712 <146841568+Monachawla1712@users.noreply.github.com>
2023-12-08 12:56:27 +0530

    Update builld.sh

--- scripts/builld.sh
+++ scripts/builld.sh
@@ -3,7 +3,6 @@
 export NAME
 export USER
 export PASSWORD
-export HOST
 
 sudo sed -i "s/^.*DB_NAME.*$/define('DB_NAME', '$NAME');/" wp-config.php
 sudo sed -i "s/^.*DB_USER.*$/define('DB_USER', '$USER');/" wp-config.php
